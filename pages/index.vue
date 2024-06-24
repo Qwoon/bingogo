534618d5ef3ccfa1019498a972fc93adf7575e36
Artyom Gregory <artyom.gregory@gmail.com>
2024-06-24 16:05:05 +0300

    #11(feat): Router navigation from boards to single game

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <section class="h-screen d-flex flex-wrap justify-center align-center">
-    <GameBoard />
+    <VCard class="pa-5 ma-4" color="primary">
+      <VCardTitle
+        >Welcome to Bingogo <Icon name="mdi:gamepad-variant-outline"></Icon
+      ></VCardTitle>
+      <VCardSubtitle
+        >Create your own bingo game and play with friends</VCardSubtitle
+      >
+      <VCardText>
+        Bingogo is a free bingo constructor, which you can use to create unique
+        text games to play with your friends. Watch streams, create bingo
+        boards, close tiles and create challenges for each closed
+        tile.</VCardText
+      >
+      <VCardText> </VCardText>
+    </VCard>
   </section>
 </template>
