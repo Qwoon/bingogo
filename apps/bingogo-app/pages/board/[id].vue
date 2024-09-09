6f25239f2041a930081feffb551faf0e40fc0a3e
Artyom Gregory <artyom.gregory@gmail.com>
2024-09-09 20:16:33 +0300

    (fix): Id param from route

--- apps/bingogo-app/pages/board/[id].vue
+++ apps/bingogo-app/pages/board/[id].vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-const boardStore = useBoardStore();
-const { resource } = storeToRefs(boardStore);
+const boardStore = useBoardStore()
+const { resource } = storeToRefs(boardStore)
 
+const route = useRoute()
 onBeforeMount(async () => {
-  await boardStore.get(1);
-});
+  if (route.params.id) await boardStore.get(parseInt(route.params.id as string))
+  else navigateTo('/')
+})
 </script>
 <template>
-  <section
-    v-if="resource"
-    class="h-screen d-flex flex-wrap justify-center align-center"
-  >
+  <section v-if="resource" class="h-screen d-flex flex-wrap justify-center align-center">
     <GameBoard :tiles="resource.tiles" />
   </section>
 </template>
