6dd858dab50db99a9ff1e0c81d5492e1a74a9c86
Artyom Gregory <artyom.gregory@gmail.com>
2024-09-15 00:43:00 +0300

    #22(feat): Add blank layout to home page

--- apps/bingogo-app/pages/index.vue
+++ apps/bingogo-app/pages/index.vue
@@ -1,18 +1,20 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'blank',
+  scrollToTop: true
+})
+</script>
 <template>
   <section class="h-screen d-flex flex-wrap justify-center align-center">
     <VCard class="pa-5 ma-4" color="primary">
       <VCardTitle
-        >Welcome to Bingogo v0.0.2
-        <Icon name="mdi:gamepad-variant-outline"></Icon
+        >Welcome to Bingogo v0.0.2 <Icon name="mdi:gamepad-variant-outline"></Icon
       ></VCardTitle>
-      <VCardSubtitle
-        >Create your own bingo game and play with friends</VCardSubtitle
-      >
+      <VCardSubtitle>Create your own bingo game and play with friends</VCardSubtitle>
       <VCardText>
-        Bingogo is a free bingo constructor, which you can use to create unique
-        text games to play with your friends. Watch streams, create bingo
-        boards, close tiles and create challenges for each closed
-        tile.</VCardText
+        Bingogo is a free bingo constructor, which you can use to create unique text games to play
+        with your friends. Watch streams, create bingo boards, close tiles and create challenges for
+        each closed tile.</VCardText
       >
       <VCardText> </VCardText>
     </VCard>
