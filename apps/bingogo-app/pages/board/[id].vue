f5f5c8ac374ffee98da2d0ca12528d9ec975733c
Artyom Gregory <artyom.gregory@gmail.com>
2026-04-06 15:14:37 +0300

    Change index.vue home page

--- apps/bingogo-app/pages/board/[id].vue
+++ apps/bingogo-app/pages/board/[id].vue
@@ -5,7 +5,10 @@ const { params } = useRoute()
 const boardId = ref<string>(params.id as string)
 
 //const { data: resource } = await useApi().getBoardById(parseInt(boardId.value))
-const resource = ref<BoardTile.Props[]>([{ title: '', isChecked: false, points: 10, boardId: 1 }])
+const resource = ref<BoardTile.Props[]>([
+  { title: 'Hello world!', isChecked: false, points: 10, boardId: 1 },
+  { title: 'Hello world 2!', isChecked: false, points: 10, boardId: 1 }
+])
 </script>
 <template>
   <section v-if="resource" class="h-screen d-flex flex-wrap justify-center align-center">
--- apps/bingogo-app/pages/index.vue
+++ apps/bingogo-app/pages/index.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
-import { BoardTile } from '~/domain'
-
-//const { data: resource } = await useApi().getBoardById(parseInt(boardId.value))
-const resource = ref<BoardTile.Props[]>([
-  { title: 'Hello world!', isChecked: false, points: 10, boardId: 1 },
-  { title: 'Hello world 2!', isChecked: false, points: 10, boardId: 1 }
-])
+const router = useRouter()
 </script>
 <template>
-  <section v-if="resource" class="h-screen d-flex flex-wrap justify-center align-center">
-    <GameBoard :tiles="resource" />
+  <section class="h-screen d-flex flex-wrap justify-center align-center">
+    <VContainer max-width="480">
+      <VRow justify="center" class="mb-6">
+        <VCol cols="auto">
+          <h1 class="text-h4 font-weight-bold text-center">BingoGo</h1>
+          <p class="text-subtitle-1 text-center text-medium-emphasis">What would you like to do?</p>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol cols="12">
+          <VCard :elevation="4" class="pa-6 cursor-pointer" @click="router.push('/board/1')">
+            <div class="d-flex align-center ga-4">
+              <Icon name="ic:round-add-circle" size="40" class="text-primary" />
+              <div>
+                <div class="text-h6 font-weight-bold">Create New Board</div>
+                <div class="text-body-2 text-medium-emphasis">Set up a new bingo board to play</div>
+              </div>
+            </div>
+          </VCard>
+        </VCol>
+        <VCol cols="12">
+          <VCard :elevation="4" class="pa-6" disabled>
+            <div class="d-flex align-center ga-4">
+              <Icon name="ic:round-view-list" size="40" class="text-secondary" />
+              <div>
+                <div class="text-h6 font-weight-bold">List Boards</div>
+                <div class="text-body-2 text-medium-emphasis">
+                  Browse and load an existing board
+                </div>
+                <VChip size="x-small" class="mt-1">Coming soon</VChip>
+              </div>
+            </div>
+          </VCard>
+        </VCol>
+      </VRow>
+    </VContainer>
   </section>
 </template>
